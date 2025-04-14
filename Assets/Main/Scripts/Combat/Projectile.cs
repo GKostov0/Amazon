c108a99690ea14cefdb0b547bc0caeffecd1fdbd
Gkostov01 <georgi.kostov0101@gmail.com>
2025-04-14 23:19:46 +0300

    Add staff and dungeon
    Add Staff weapon
    Add Synty dungeon realms

--- Assets/Main/Scripts/Combat/Projectile.cs
+++ Assets/Main/Scripts/Combat/Projectile.cs
@@ -1,69 +1,87 @@
 using AMAZON.Core;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+namespace AMAZON.Combat
 {
-    [SerializeField][Range(2.0f, 45.0f)] private float _speed = 9.0f;
-    [SerializeField] private Collider _collider;
-    [SerializeField] private bool _isHoaming;
-
-    private Health _target = null;
-    private float _damage = 0;
-    private float _targetOffset;
-    private Vector3 _targetPosition;
-
-    private bool FlipCoin() => Random.value < 0.5f;
-    private float GetOffset() => Random.Range(0.1f, 1.0f);
-    private float GetRandomOffset() => FlipCoin() ? GetOffset() : -GetOffset();
-
-    private void Start()
+    public class Projectile : MonoBehaviour
     {
-        _targetOffset = GetRandomOffset();
-        _targetPosition = Vector3.up * _targetOffset;
+        [SerializeField] private GameObject _trailEffect;
+        [SerializeField] private GameObject _explosionEffect;
+        [SerializeField][Range(2.0f, 45.0f)] private float _speed = 9.0f;
+        [SerializeField] private Collider _collider;
+        [SerializeField] private bool _isHoaming;
+        [SerializeField] private bool _destroyTrailOnImpact;
 
-        transform.LookAt(GetAimLocation());
-        Destroy(gameObject, 10.0f);
-    }
+        private Health _target = null;
+        private float _damage = 0;
+        private float _targetOffset;
+        private Vector3 _targetPosition;
 
-    private void Update()
-    {
-        if (_target == null) return;
+        private bool FlipCoin() => Random.value < 0.5f;
+        private float GetOffset() => Random.Range(0.3f, 0.7f);
+        private float GetRandomOffset() => FlipCoin() ? GetOffset() : -GetOffset();
 
-        if (_isHoaming && !_target.IsDead())       
-            transform.LookAt(GetAimLocation());
-
-        transform.Translate(_speed * Time.deltaTime * Vector3.forward);
-    }
-
-    public void SetTarget(Health target, float damage)
-    {
-        _target = target;
-        _damage = damage;
-    }
-
-    private Vector3 GetAimLocation()
-    {
-        Collider collider = _target.GetComponent<Collider>();
-        return collider ? collider.bounds.center + _targetPosition : _target.transform.position;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Health health = other.GetComponent<Health>();
-        if (health != _target) return;
-
-        if (other.CompareTag("Enemy") || other.CompareTag("Player"))
+        private void Start()
         {
-            _target.TakeDamege(_damage);
-            transform.parent = health.GetModel();
-            _target = null;
-            _collider.enabled = false;
-            if (health.IsDead())
-            {
-                other.enabled = false;
-            }
+            _targetOffset = GetRandomOffset();
+            _targetPosition = Vector3.up * _targetOffset;
+
+            transform.LookAt(GetAimLocation());
+            Destroy(gameObject, 10.0f);
         }
 
-        Destroy(gameObject, 5.0f);
+        private void Update()
+        {
+            if (_target == null) return;
+
+            if (_isHoaming && !_target.IsDead())
+                transform.LookAt(GetAimLocation());
+
+            transform.Translate(_speed * Time.deltaTime * Vector3.forward);
+        }
+
+        public void SetTarget(Health target, float damage)
+        {
+            _target = target;
+            _damage = damage;
+        }
+
+        private Vector3 GetAimLocation()
+        {
+            Collider collider = _target.GetComponent<Collider>();
+            return collider ? collider.bounds.center + _targetPosition : _target.transform.position;
+        }
+
+        private void HandlePArticleEffects()
+        {
+            if (_destroyTrailOnImpact && _trailEffect)
+                Destroy(_trailEffect, 0.5f);
+
+            if (_explosionEffect)
+                _explosionEffect.SetActive(true);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Health health = other.GetComponent<Health>();
+            if (health != _target) return;
+
+            if (other.CompareTag("Enemy") || other.CompareTag("Player"))
+            {
+                HandlePArticleEffects();
+
+                _target.TakeDamege(_damage);
+                transform.parent = health.GetModel();
+
+                _speed = 0.0f;
+                _target = null;
+                _collider.enabled = false;
+
+                if (health.IsDead())
+                    other.enabled = false;
+            }
+
+            Destroy(gameObject, 5.0f);
+        }
     }
 }
